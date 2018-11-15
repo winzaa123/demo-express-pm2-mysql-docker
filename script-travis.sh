fbeef1cf8b16bad758e80574c148cd4ee54052f1
win <win@directrooms.com>
2018-11-15 16:19:53 +0700

    Update script and test sh.

--- script-travis.sh
+++ script-travis.sh
@@ -10,4 +10,6 @@ docker exec -it node_app_test sh -c "ping  -c 4 nginx_express_phpmyadmin"
 docker exec -it node_app_test sh -c "ping  -c 4 node_mariadb_test"
 docker exec -it node_app_test sh -c "nc -z -v node_mariadb_test 3306"
 docker logs node_app_test
-curl -s http://localhost:8000/test --connect-timeout 20  --progress-bar | cat # output json write data in mysql
+# curl -s http://localhost:8000/test --connect-timeout 20  --progress-bar | cat # output json write data in mysql
+while ! curl -s http://localhost:8000/test --connect-timeout 20  --progress-bar ; do sleep 1 ; done
+docker exec -it node_mariadb_test sh -c "mysql -u $MARIADB_USER -p$MARIADB_PASSWORD -e 'SHOW DATABASES; USE test_db; SELECT * from user_knex;'"
